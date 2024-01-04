3c62c0084f0f1a89c6344bf6b52b384263e8d177
tiennv <tiennv.110@gmail.com>
2024-01-04 23:08:35 +0700

    update test2

--- test2.py
+++ test2.py
@@ -9,11 +9,13 @@ base_url = 'https://shopee.vn'
 
 def transform(r):
     try:
+        # trả về danh sách theo định dạng yêu cầu
         return (r['name'],
                 r['price'],
                 json.loads(r['item_rating'].replace("'", '"'))['rating_star'],
                 r['price'] * r['historical_sold'],
-                base_url + '/' + urllib.parse.quote_plus(r['name'].replace(' ', '-') + f'-i.{r["shopid"]}.{r["itemid"]}'),
+                base_url + '/' + urllib.parse.quote_plus(
+                    r['name'].replace(' ', '-') + f'-i.{r["shopid"]}.{r["itemid"]}'),
                 r['itemid'], r['catid'])
     except Exception as e:
         print(r.to_dict())
@@ -22,8 +24,9 @@ def transform(r):
 
 if __name__ == '__main__':
     # test 2
-    df_data = pd.read_csv('data/data.csv', engine='pyarrow', dtype_backend='pyarrow')
     list_data = []
+
+    # chuyển file json các nhóm mặt hàng sang dạng dataframe
     with open('data/category.json', 'r') as f:
         data = json.loads(f.read())
 
@@ -35,28 +38,41 @@ if __name__ == '__main__':
     df_category = pd.DataFrame.from_records(list_data)
     df_category.drop(axis=1, columns=['children'])
 
+    # đọc dữ liệu raw
+    df_data = pd.read_csv('data/data.csv', engine='pyarrow', dtype_backend='pyarrow')
     print("khối lượng dữ liệu raw:", df_data.shape)
+
+    # thực hiện transform
+    list_field = ['product_name', 'product_price', 'product_rating', 'product_url', 'product_revenue']
     s = time.time()
-    df_data[['product_name', 'product_url', 'product_rating', 'product_price', 'product_revenue', 'itemid', 'catid']] = df_data.apply(
-        transform, axis=1, result_type='expand')
-    df_formatted_data = df_data[['product_name', 'product_price', 'product_rating', 'product_url', 'product_revenue']]
+    # thực hiện lưu lại itemid và catid để phục vụ việc kiểm tra trùng lặp
+    df_data[list_field + ['itemid', 'catid']] = df_data.apply(transform, axis=1, result_type='expand')
+    df_formatted_data = df_data[list_field]
+
+    # lưu lại vào file csv như yêu cầu
     df_formatted_data.to_csv('data/formatted_data.csv', index=False)
     end = time.time()
     print(end - s)
-    print("Số lượng sản phẩm transform trong một phút", df_formatted_data.shape[0] / ((end - s) / 60))
+    print("============> Số lượng sản phẩm transform trong một phút", df_formatted_data.shape[0] / ((end - s) / 60))
 
-    df_full_data = df_data[
-        ['product_name', 'product_price', 'product_rating', 'product_url', 'product_revenue', 'itemid', 'catid',
-         'crawl_catid']].copy()
-    df_full_data = df_full_data.merge(df_category, how='left', left_on='crawl_catid', right_on='catid',
-                                      suffixes=('', '_category'))
+    # thực hiện một vài thống kê để kiểm tra dữ liệu
+    df_data_with_cat = df_data[list_field + ['itemid', 'catid', 'crawl_catid', 'shopid']].copy()
+    df_data_with_cat = df_data_with_cat.merge(df_category, how='left', left_on='crawl_catid', right_on='catid',
+                                              suffixes=('', '_category'))
 
+    # đếm số lượng sản phẩm trong mỗi nhóm mặt hàng lớn
     count = 0
+    not_duplicate_data_count = 0
     for i, row in df_category[df_category['level'] == 1].iterrows():
-        x = df_full_data[
-            (df_full_data['catid_category'] == row['catid']) | (df_full_data['parent_catid'] == row['catid'])]
+        x = df_data_with_cat[
+            (df_data_with_cat['catid_category'] == row['catid']) | (df_data_with_cat['parent_catid'] == row['catid'])]
         count += 1
+        x_not_duplicate = x[['itemid', 'crawl_catid']].drop_duplicates()
+
         print(f"{count}. Số lượng mặt hàng {row['display_name']} là:", x.shape[0], ". Số lượng không trùng:",
-              x['itemid'].drop_duplicates().shape[0])
-        if x['itemid'].drop_duplicates().shape[0] < 3000:
+              x_not_duplicate.shape[0])
+        if x_not_duplicate.shape[0] < 3000:
             print("----------------- WARNING ------------: số lượng mặt hàng không thỏa mãn yêu cầu đề bài")
+        not_duplicate_data_count += x_not_duplicate.shape[0]
+
+    print("tổng số lượng dữ liệu bị trùng:", df_data.shape[0] - not_duplicate_data_count)
